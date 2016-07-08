10eb6d2e432a0acf49765e74c614eec0299d0e7d
Club15 s15t233 SHIMIZU Takeru <wakatakeru@boulez2.eng.kagawa-u.ac.jp>
2016-07-08 19:48:46 +0900

    ノートの新規作成に関わるコントローラを作成

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,4 +8,12 @@ class NotesController < ApplicationController
     @note = Note.new
   end
 
+  def create
+    note = Note.new
+    note.title   = params['note']['title']
+    note.content = params['note']['content']
+    note.save
+    redirect_to notes_path
+  end
+
 end
